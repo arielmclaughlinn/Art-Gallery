c01eac48a61fd57f694cf29fac6c43b3962ace87
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-12 12:28:19 -0700

    variable for tableView created. Loaded the assets into an image array and created an empty array of Paintings.

--- Art-Gallery/Art-Gallery/PaintingModel.swift
+++ Art-Gallery/Art-Gallery/PaintingModel.swift
@@ -1,2 +1,34 @@
 import Foundation
 import UIKit
+
+class PaintingModel: NSObject, UITableViewDataSource, PaintingTableViewCellDelegate {
+    
+    weak var tableView: UITableView?
+    
+    var paintings: [Painting] = []
+    
+    override init() {
+        for numbers in 1...12 {
+            let images = ("Image\(numbers)")
+            guard let image = UIImage(named: images) else { return }
+            let thePainting = Painting(image: image)
+            paintings.append(thePainting)
+        }
+    }
+    
+    
+
+    func tappedLikeButton(on cell: PaintingTableViewCell) {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
