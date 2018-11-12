3b14e5ac86ea28845032badf2dc4906be1a1dd33
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-12 12:04:10 -0700

    Created struct and initializer. Added the protocol.

--- Art-Gallery/Art-Gallery/Painting.swift
+++ Art-Gallery/Art-Gallery/Painting.swift
@@ -1,3 +1,17 @@
 import UIKit
 
+struct Painting {
+    let image: UIImage
+    var isLiked: Bool
+    
+    init(image: UIImage, isLiked: Bool = false) {
+        self.image = image
+        self.isLiked = isLiked
+    
+    }
+    
+}
 
+protocol PaintingTableViewCellDelegate: class {
+    func tappedLikeButton(on cell: PaintingTableViewCell)
+}
