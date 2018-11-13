53705b42046c50b9a8dca4a115b0c925b103f12c
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-12 19:54:02 -0700

    Finished project

--- Art-Gallery/Art-Gallery/PaintingModel.swift
+++ Art-Gallery/Art-Gallery/PaintingModel.swift
@@ -3,14 +3,15 @@ import UIKit
 
 class PaintingModel: NSObject, UITableViewDataSource, PaintingTableViewCellDelegate {
     
-    weak var tableView: UITableView?
+    @IBOutlet weak var tableView: UITableView!
     
     var paintings: [Painting] = []
     
+    
     override init() {
-        for numbers in 1...12 {
-            let images = ("Image\(numbers)")
-            guard let image = UIImage(named: images) else { return }
+        for number in 1...12 {
+            let imageName = ("Image\(number)") // This will be a string with the format "Image1"
+            guard let image = UIImage(named: imageName) else { return }
             let thePainting = Painting(image: image)
             paintings.append(thePainting)
         }
@@ -19,30 +20,45 @@ class PaintingModel: NSObject, UITableViewDataSource, PaintingTableViewCellDeleg
     func tappedLikeButton(on cell: PaintingTableViewCell) {
         guard let indexPath = tableView?.indexPath(for: cell)
             else { fatalError("Cell does not exist.") }
-        
+
         paintings[indexPath.row].isLiked.toggle()
         
+        var newButtonTitle: String
+        if paintings[indexPath.row].isLiked {
+            newButtonTitle = "💔"
+        } else {
+            newButtonTitle = "❤️"
+        }
+        
+        cell.likeButton.setTitle(newButtonTitle, for: .normal)
+
     }
+
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        self.tableView = tableView
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return paintings.count
-
     }
-    
+
     let reuseIdentifier = "cell"
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? PaintingTableViewCell else {
             fatalError("Could not cast to cell")
         }
         
+        cell.delegate = self
+        cell.portraitView.image = paintings[indexPath.row].image
+        let title = "❤️"
+        cell.likeButton?.setTitle(title, for: .normal)
+        cell.likeButton.alpha = paintings[indexPath.row].isLiked ? 1.0 : 0.5
+        
         return cell
-
+        
     }
+   
 
     
     
--- Art-Gallery/Art-Gallery/PaintingViewController.swift
+++ Art-Gallery/Art-Gallery/PaintingViewController.swift
@@ -4,13 +4,4 @@ class PaintingViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
-        
-    }
-    
-    
-
 }
